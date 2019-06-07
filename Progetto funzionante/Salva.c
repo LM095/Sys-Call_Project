fb8222d9a01270b66ffb33b0fd6bf265dda18a5e
Luca Marzari <50518316+LM095@users.noreply.github.com>
2019-06-07 21:01:49 +0200

    Update Salva.c

--- Progetto funzionante/Salva.c
+++ Progetto funzionante/Salva.c
@@ -13,7 +13,6 @@ int main (int argc, char *argv[])
     printf("Hi, I'm Salva program!\n");
 
     char buffer[MAX_READ]=" ";
-    ssize_t numRead = 0;
     int i = 0,j = 0, w = 0;
 
     for(i = 0; i < argc; i++)
@@ -22,12 +21,12 @@ int main (int argc, char *argv[])
         {
             buffer[w] = argv[i][j];
             w++;
-            numRead++;
         }
         buffer[w] = ' ';
         w++;
     }
     buffer[w]= '\0';
+    
     int destinazione= open("salva.txt", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     write(destinazione, buffer, strlen(buffer));
         
